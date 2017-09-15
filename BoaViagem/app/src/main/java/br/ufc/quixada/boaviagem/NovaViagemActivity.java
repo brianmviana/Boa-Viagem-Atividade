1be3276ccc5000702d912f3ec86101c3db144e11
Antonio Raimundo <antonioraimundo007@gmail.com>
2017-09-15 15:00:50 -0300

    teste de git

---
+++ BoaViagem/app/src/main/java/br/ufc/quixada/boaviagem/NovaViagemActivity.java
@@ -0,0 +1,13 @@
+package br.ufc.quixada.boaviagem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NovaViagemActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nova_viagem);
+    }
+}
